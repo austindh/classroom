181f5474c729ec77a83287ba99fe789b7d76c448
Austin Hughes <austindavidhughes@gmail.com>
2016-10-28 20:19:56 -0600

    Initial page working!

---
+++ site.js
@@ -0,0 +1,52 @@
+let students = [
+	'Michael',
+	'Gage',
+	'Melissa',
+	'Jaylynn',
+	'Rhylind',
+	'Shaylie',
+	'Jack',
+	'Chaille',
+	'Sami',
+	'Tyler',
+	'Rose',
+	'Parker',
+	'Alex',
+	'Shyah',
+	'Rosabelle',
+	'Edgar',
+	'Anthony',
+	'David',
+	'Serenity',
+	'Valentin'
+];
+
+$( document ).ready( () => {
+
+	let rows = [];
+	let row = [];
+	for ( let i = 0; i < students.length; i++ ) {
+		if ( row.length >= 7 ) {
+			rows.push( row );
+			row = [];
+		}
+		row.push( students[i] );
+	}
+	rows.push( row );
+
+	console.log( rows );
+
+	rows.forEach( row => {
+		studentHtml = row.map( student => `<div class="student">${ student }</div>`)
+		$( '.students' ).append(`<div class="student-row">${ studentHtml.join('\n') }</div>`)
+	});
+
+	$( '.student' ).on( 'click', e => {
+		$( e.target ).toggleClass( 'done' );
+	});
+
+	$( '#reset' ).on( 'click', e => {
+		$( '.student.done' ).removeClass( 'done' );
+	});
+
+});
